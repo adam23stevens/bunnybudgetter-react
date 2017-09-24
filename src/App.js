c0c57af8f6edb149589168e8260c31dc4b579c05
Adam <adam23stevens@gmail.com>
2017-09-24 18:58:33 +0100

    code first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>
+        Hi there bunny budgetter ! :)
+      </h1>
+    );
+  }
+}
+
+export default App;
